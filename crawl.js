6fc8283d294c3f464be85620051a6881d2728ef7
Kamil <kamilttt271@gmail.com>
2024-05-02 09:52:21 +0200

    Add recursive crawling functionality

--- crawl.js
+++ crawl.js
@@ -1,25 +1,43 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentUrl) {
+async function crawlPage(baseUrl, currentUrl, pages) {
+    const baseUrlObj = new URL(baseUrl)
+    const currentUrlObj = new URL(currentUrl)
+    if (baseUrlObj.hostname !== currentUrlObj.hostname) {
+        return pages
+    }
+    const currentUrlNormalized = normalizeURL(currentUrl)
+    if (pages[currentUrlNormalized] > 0) {
+        pages[currentUrlNormalized]++
+        return pages
+    } 
+    pages[currentUrlNormalized] = 1
+
     console.log(`actively crawling ${currentUrl}`)
     try {
         const resp = await fetch(currentUrl)
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status}, on page ${currentUrl}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes('text/html')) {
             console.log(`error in fetch, content type: ${contentType}, on page: ${currentUrl}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        const internalUrls = getURLsFromHTML(htmlBody, baseUrl) // Bug with using current instead of baseUrl
+
+        for (const url of internalUrls) {
+            pages = await crawlPage(baseUrl, url, pages)
+        }
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentUrl}`)
     }
-    
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, path) {
@@ -56,9 +74,9 @@ function normalizeURL(url) {
     const urlObj = new URL(url)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
     if (hostPath.length && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1);
+        return hostPath.slice(0, -1)
     }
-    return hostPath;
+    return hostPath
 }
 
 module.exports = {
